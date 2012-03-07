6104a11cc5d0176c959c84a06f3a672463c56583
Frog Cherry <frogcherry@gmail.com>
2012-03-07 21:39:07 +0800

    错误的解决方案，应该把事件绑定给上一级

--- src/main/java/com/ooobgy/mapinfo/TestLay.java
+++ src/main/java/com/ooobgy/mapinfo/TestLay.java
@@ -1,12 +1,77 @@
 package com.ooobgy.mapinfo;
 
+import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.MouseInputListener;
+
+import com.ooobgy.mapinfo.ui.MapInfoDisplay;
 
 public class TestLay extends JFrame {
+    private class DisplayMouseListenner implements MouseInputListener {
+            private Point prePt = new Point();
+    
+            @Override
+            public void mousePressed(MouseEvent e) {
+                refreshPrePt(e);
+    //            System.out.println(e.getX());
+                System.out.println(e.getX());
+            }
+    
+            private void refreshPrePt(MouseEvent e) {
+                prePt.x = e.getX();
+                prePt.y = e.getY();
+            }
+    
+            @Override
+            public synchronized void mouseDragged(MouseEvent e) {
+                System.out.println(e.getX());
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    int dx = e.getX() - prePt.x;
+                    int dy = e.getY() - prePt.x;
+                    // System.out.println(dx + "#" + dy);
+                    // Point dispLoc = MapInfoDisplay.this.getLocat ion();
+                    TestLay.this.jButton.setLocation(60+dx, 25+dx);
+                }
+            }
+    
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+    
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+    
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+    
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+    
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                // TODO Auto-generated method stub
+                System.out.println("M");
+            }
+    
+        }
+
     private JPanel jContentPane = null;
     private JButton jButton = null;
     /**
@@ -37,6 +102,7 @@ public class TestLay extends JFrame {
      this.setSize(300, 200);
      this.setTitle("JFrame");
      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     this.addMouseListener(new DisplayMouseListenner());
      this.setVisible(true);
     }
     
--- src/main/java/com/ooobgy/mapinfo/ui/MapInfoDisplay.java
+++ src/main/java/com/ooobgy/mapinfo/ui/MapInfoDisplay.java
@@ -1,6 +1,10 @@
 package com.ooobgy.mapinfo.ui;
 
+import java.awt.Component;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+
+import javax.swing.SwingUtilities;
 import javax.swing.event.MouseInputListener;
 
 import com.ooobgy.mapinfo.conf.Config;
@@ -8,24 +12,24 @@ import com.ooobgy.mapinfo.consts.ConfConsts;
 
 import prefuse.Display;
 import prefuse.Visualization;
+import prefuse.controls.ControlAdapter;
 import prefuse.data.Table;
 import prefuse.data.io.CSVTableReader;
 import prefuse.data.io.DataIOException;
 import prefuse.data.util.TableIterator;
 
 /**
- * The main display (view) of the map demo.</br>
- * Before to use, it should be {@link #init(String)} with data file first.
- * <b>created:</b> 2012-3-5
- * @author 周晓龙  frogcherry@gmail.com
+ * The main display (view) of the map demo.</br> Before to use, it should be
+ * {@link #init(String)} with data file first. <b>created:</b> 2012-3-5
+ * 
+ * @author 周晓龙 frogcherry@gmail.com
  */
-public class MapInfoDisplay extends Display{
+public class MapInfoDisplay extends Display {
     public MapInfoDisplay() {
         super(new Visualization());
     }
 
     private Table data;
-    
 
     /**
      * Random Serial Version UID
@@ -34,39 +38,91 @@ public class MapInfoDisplay extends Display{
 
     public void init(String mapDataFile) throws DataIOException {
         this.data = (new CSVTableReader()).readTable(mapDataFile);
-        
+
         setBackgroundImage(Config.get(ConfConsts.BK_IMG_FILE), false, false);
-        
-        for( TableIterator iterator = this.data.iterator(); iterator.hasNext();){
-            //System.out.println(Config.get(ConfConsts.DATA_NAME));
+
+        for (TableIterator iterator = this.data.iterator(); iterator.hasNext();) {
+            // System.out.println(Config.get(ConfConsts.DATA_NAME));
             iterator.nextInt();
-            System.out.println(iterator.get(Config.get(ConfConsts.DATA_NAME)));
-            //System.out.println(iterator.get("error"));
+//            System.out.println(iterator.get(Config.get(ConfConsts.DATA_NAME)));
+            // System.out.println(iterator.get("error"));
         }
         // TODO Auto-generated method stub
-        this.setSize(Config.getInt(ConfConsts.FRAME_WIDTH), Config.getInt(ConfConsts.FRAME_HEIGHT));
+        this.setSize(Config.getInt(ConfConsts.FRAME_WIDTH),
+                Config.getInt(ConfConsts.FRAME_HEIGHT));
 
         bindEventListener();
-        this.addMouseListener(new DisplayMouseListenner());
-        
-    }
-    
-    private void bindEventListener() {
-        addMouseListener(new DisplayMouseListenner());
+
     }
 
-    class DisplayMouseListenner implements MouseInputListener {
+    private void bindEventListener() {
+        
+//         addMouseListener(new DisplayMouseListenner());
+        this.addControlListener(new MapInfoControl());
+    }
+
+    private class MapInfoControl extends ControlAdapter {
+        private Point prePt = new Point();
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            refreshPrePt(e);
+//            System.out.println(e.getX());
+        }
+
+        private void refreshPrePt(MouseEvent e) {
+            prePt.x = e.getX();
+            prePt.y = e.getY();
+        }
+
+        @Override
+        public synchronized void mouseDragged(MouseEvent e) {
+            
+            if (SwingUtilities.isLeftMouseButton(e)) {
+                Component c = e.getComponent();
+                int dx = e.getX() - prePt.x;
+                int dy = e.getY() - prePt.x;
+                System.out.println(dx);
+                // System.out.println(dx + "#" + dy);
+                // Point dispLoc = MapInfoDisplay.this.getLocat ion();
+                c.setLocation(dx, dy);
+                c.repaint();
+            }
+        }
+
+    }
+
+    private class DisplayMouseListenner implements MouseInputListener {
+        private Point prePt = new Point();
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            refreshPrePt(e);
+//            System.out.println(e.getX());
+        }
+
+        private void refreshPrePt(MouseEvent e) {
+            prePt.x = e.getX();
+            prePt.y = e.getY();
+        }
+
+        @Override
+        public synchronized void mouseDragged(MouseEvent e) {
+            System.out.println(e.getX());
+            if (SwingUtilities.isLeftMouseButton(e) &&(e.getX() >= prePt.x)) {
+                int dx = e.getX() - prePt.x;
+                int dy = e.getY() - prePt.x;
+                // System.out.println(dx + "#" + dy);
+                refreshPrePt(e);
+                 Point dispLoc = MapInfoDisplay.this.getLocation();
+                MapInfoDisplay.this.setLocation(dispLoc.x + dx, dispLoc.y + dy);
+            }
+        }
 
         @Override
         public void mouseClicked(MouseEvent e) {
             // TODO Auto-generated method stub
             
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-            System.out.println(e.getX());
-            System.out.println(MapInfoDisplay.this.getLocation());
         }
 
         @Override
@@ -88,15 +144,11 @@ public class MapInfoDisplay extends Display{
         }
 
         @Override
-        public void mouseDragged(MouseEvent e) {
-            
-        }
-
-        @Override
         public void mouseMoved(MouseEvent e) {
             // TODO Auto-generated method stub
             
         }
-    } 
+
+    }
 
 }
